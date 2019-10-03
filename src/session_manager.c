01cb19c53fefd547b14a7f234da315260b084b3b
Paul Guillier <paul.guillier@protonmail.com>
2019-10-03 23:34:58 +0200

    wip user login refacto #1

--- src/session_manager.c
+++ src/session_manager.c
@@ -6,7 +6,7 @@
 /*   By: marvin <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/27 11:23:05 by marvin            #+#    #+#             */
-/*   Updated: 2019/10/03 11:27:59 by pguillie         ###   ########.fr       */
+/*   Updated: 2019/10/03 23:32:43 by pguillie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,52 +19,67 @@
 #include "protocol_interpreter.h"
 #include "../libft/include/libft.h"
 
-static int init_session(struct ftp_session *session, int sock,
-	struct sockaddr_in addr)
-{
-	session->lnmax = sysconf(_SC_LOGIN_NAME_MAX);
-	if (lnmax == -1)
-		lnmax = 128;
-	session->user.login = malloc(lnmax);
-	session->user.name = malloc(lnmax);
-	if (session->user.login == NULL || session->user.name == NULL)
-		return -1;
-	ft_memset(&(session->user.login), '\0', lnmax);
-	ft_memset(&(session->user.name), '\0', lnmax);
-	session->user.uid = 0;
-	session->user.gid = 0;
-	session->control = (struct connection){addr, sock};
-	session->data_type = TYPE_ASCII;
-	if (send_dtp_reply(session->control.sock, FTP_CONN_CTRL_READY) != 0)
-		return -1;
-	return 0;
-}
+/* static int init_session(struct ftp_session *session, int sock, */
+/* 	struct sockaddr_in addr) */
+/* { */
+/* 	session->lnmax = sysconf(_SC_LOGIN_NAME_MAX); */
+/* 	if (lnmax == -1) */
+/* 		lnmax = 128; */
+/* 	session->user.login = malloc(lnmax); */
+/* 	session->user.name = malloc(lnmax); */
+/* 	if (session->user.login == NULL || session->user.name == NULL) */
+/* 		return -1; */
+/* 	ft_memset(&(session->user.login), '\0', lnmax); */
+/* 	ft_memset(&(session->user.name), '\0', lnmax); */
+/* 	session->user.uid = 0; */
+/* 	session->user.gid = 0; */
+/* 	session->control = (struct connection){addr, sock}; */
+/* 	session->data_type = TYPE_ASCII; */
+/* 	if (send_dtp_reply(session->control.sock, FTP_CONN_CTRL_READY) != 0) */
+/* 		return -1; */
+/* 	return 0; */
+/* } */
 
 int session_manager(int sock, struct sockaddr_in addr)
 {
 	struct ftp_session session;
 	int status, pipefd[2];
-	pid_t user;
+	pid_t pi;
+
+	struct passwd user;
+	char *login = malloc(128);
 
 	if (signal(SIGCHLD, SIG_DFL) == SIG_ERR) /* sth more adapted? */
 		exit(EXIT_FAILURE);
-	if (init_session(&session, sock, addr) == -1)
-		die(&session);
+	/* if (init_session(&session, sock, addr) == -1) */
+	/* 	die(&session); */
 	while (1) {
 		printf("fork!\n");
 		if (pipe(pipefd) == -1)
-			die(&session);
-		user = fork();
-		if (user < 0) {
-			die(&session);
-		} else if (user == 0) {
+			exit(1);//die(&session);
+		user = get_user_info(login); /* struct passwd or similar so that
+						father knows which user is logged in child */
+		pi = fork();
+		if (pi < 0) {
+			exit(1);//die(&session);
+		} else if (pi == 0) {
 			close(pipefd[0]);
-			session.pipefd = pipefd[1];
-			protocol_interpreter(&session);
+			//session.pipefd = pipefd[1];
+			log_user(login, username);
+			/* if successfull:
+			   set 'dir' to home
+			   else:
+			   'dir' is the old working_dir */
+			protocol_interpreter(sock, addr, pipefd[1], dir);//(&session);
 		} else {
+			/* read:
+			   login (string) -- user to log in
+			   working_directory (string) -- idem
+			   transfer_parameters (stream passing through) -- settings to be kept in new process
+			 */
 			ssize_t n;
 			close(pipefd[1]);
-			wait4(user, &status, 0, NULL);
+			wait4(pi, &status, 0, NULL);
 			printf("Child exited with status: %d\n", status);
 			if (!WIFEXITED(status) || status != EXIT_SUCCESS)
 				break ;
