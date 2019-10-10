b798045a345f8a3fc777531d36ebf274af02b31f
Paul Guillier <paul.guillier@protonmail.com>
2019-10-10 11:21:11 -0400

    WIP user login refacto

--- src/auth.c
+++ src/auth.c
@@ -6,7 +6,7 @@
 /*   By: pguillie <pguillie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/05 11:34:34 by pguillie          #+#    #+#             */
-/*   Updated: 2019/10/05 13:37:17 by pguillie         ###   ########.fr       */
+/*   Updated: 2019/10/10 11:19:44 by pguillie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,22 @@
 
 #include <stdio.h> //printf
 
+static int get_session_login(int fd, struct ftp_session *session, char *login)
+{
+	ssize_t n;
+
+	n = read(fd, session, sizeof(struct ftp_session));
+	if (n == 0)
+		return 0;
+	else if (n < 0 || (size_t)n < sizeof(struct ftp_session))
+		return -1;
+	n = read(fd, login, sizeof(login));
+	if (n < 1)
+		return -1;
+	login[n] = '\0';
+	return 1;
+}
+
 static char *get_password(struct ftp_session *session)
 {
 	char line[128];//
@@ -33,9 +49,10 @@ static char *get_password(struct ftp_session *session)
 		return NULL;
 	}
 	set_command(session, line);
-	if (session->command == &ftp_pass && session->args != NULL)
+	if (session->command != &ftp_pass || session->args == NULL) {
+		send_reply(session->control.sock, FTP_AUTH_USAGE);
 		return NULL;
-	send_reply(session->control.sock, FTP_AUTH_USAGE);
+	}
 	return ft_strdup(session->args);
 }
 
@@ -49,27 +66,37 @@ int auth(int pipefd, struct ftp_session *session, struct passwd **pwd_ptr)
 
 	*pwd_ptr = NULL;
 	ret = get_session_login(pipefd, session, login);
+	printf("get login: %s\n", login);
 	if (ret == 1) {
-		printf("get login: %s\n", login);
-		passwd = get_password(session);
-		printf("passwd: %s\n", passwd);
 		pwd = getpwnam(login);
 		spwd = getspnam(login);
-		if (pwd == NULL || spwd == NULL) {
-			printf("NULL struct!\n");
-			ft_memset(passwd, '\0', ft_strlen(passwd));
-			free(passwd);
-			return ret;
+		passwd = get_password(session);
+		printf("passwd: %s\n", passwd);
+		if (pwd != NULL) {
+			write(1, "foo\n", 4);
+			if (spwd != NULL)
+				pwd->pw_passwd = spwd->sp_pwdp;
+			else
+				perror("shadow");
+			encrypted = crypt(passwd, pwd->pw_passwd);
+			printf("enc: %s\npwd: %s\n", encrypted, pwd->pw_passwd);
+		} else {
+			write(1, "bar\n", 4);
+			encrypted = NULL;
 		}
-		if (spwd != NULL)
-			pwd->pw_passwd = spwd->sp_pwdp;
-		encrypted = crypt(passwd, pwd->pw_passwd);
+		write(1, "baz\n", 4);
 		ft_memset(passwd, '\0', ft_strlen(passwd));
 		free(passwd);
-		if (ft_strcmp(encrypted, pwd->pw_passwd) == 0)
+		write(1, "A\n", 2);
+		if (encrypted && ft_strcmp(encrypted, pwd->pw_passwd) == 0) {
 			*pwd_ptr = pwd;
-		else
-			printf("Wrong password?\n");
-	}
+			send_reply(session->control.sock, FTP_AUTH_OK);
+		} else {
+			printf("Wrong password\n");
+			send_reply(session->control.sock, FTP_AUTH_ERR);
+		}
+		write(1, "B\n", 2);
+	} else if (ret == -1)
+		send_reply(session->control.sock, FTP_CONN_CTRL_ERR);
 	return ret;
 }
--- src/session_manager.c
+++ src/session_manager.c
@@ -6,7 +6,7 @@
 /*   By: marvin <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/27 11:23:05 by marvin            #+#    #+#             */
-/*   Updated: 2019/10/05 13:33:39 by pguillie         ###   ########.fr       */
+/*   Updated: 2019/10/10 10:31:33 by pguillie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,35 +34,13 @@ static int init_session(struct ftp_session *session, struct connection control)
 
 static int login(struct passwd *pwd)
 {
-	if (pwd->pw_uid == 0
-		|| setgid(pwd->pw_gid) < 0
+	if (!pwd || pwd->pw_uid == 0)
+		return 0;
+	if (setgid(pwd->pw_gid) < 0
 		|| initgroups(pwd->pw_name, pwd->pw_gid) < 0
 		|| setuid(pwd->pw_uid) < 0
 		|| chdir(pwd->pw_dir) < 0)
 		return -1;
-	return 0;
-}
-
-static int get_session(int pipefd, struct ftp_session *session)
-{
-	ssize_t n;
-
-	n = read(pipefd, session, sizeof(struct ftp_session));
-	if (n == 0)
-		return 0;
-	else if (n < 0 || (size_t)n < sizeof(struct ftp_session))
-		return -1;
-	return 1;
-}
-
-static struct passwd *get_session_login(int pipefd, struct ftp_session *session)
-{
-	ssize_t n;
-
-	n = read(pipefd, login, sizeof(login));
-	if (n < 1)
-		return -1;
-	login[n] = '\0';
 	return 1;
 }
 
@@ -86,30 +64,16 @@ int session_manager(int sock, struct sockaddr_in addr)
 			die(&session);
 		} else if (pi == 0) {
 			close(pipefd[0]);
-			if (!pwd)
-				session.auth = 0;
-			else if (login(pwd) < 0)
+			session.auth = login(pwd);
+			if (session.auth < 0)
 				die(&session);
-			else
-				session.auth = 1;
  			session.pipefd = pipefd[1];
 			protocol_interpreter(&session);
 		}
 		close(pipefd[1]);
 		wait4(pi, &ret, 0, NULL);
-		printf("Child exited with status: %d\n", ret);
 		if (WIFEXITED(ret) && ret == EXIT_SUCCESS) {
-			ret = get_session(pipefd[0], &session);
-			quit = (ret == 1 ? 0 : 1);
-
-
-
 			ret = auth(pipefd[0], &session, &pwd);
-			if (ret == -1)
-				send_reply(session.control.sock, FTP_CONN_CTRL_ERR);
-			else if (ret == 1)
-				send_reply(session.control.sock,
-					pwd ? FTP_AUTH_OK : FTP_AUTH_ERR);
 			quit = (ret == 1 ? 0 : 1);
 		} else {
 			quit = 1;
